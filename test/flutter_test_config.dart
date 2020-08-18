f3ede983dc2e3b96e24ef8c8c71b67e42dadb27b
Abhilash Chandran <csc.abhilash@gmail.com>
2020-08-18 22:43:31 +0200

    Fixes path in the test config to be OS agnostic.

--- test/flutter_test_config.dart
+++ test/flutter_test_config.dart
@@ -4,6 +4,7 @@ import 'dart:typed_data';
 
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:path/path.dart' as p;
 
 Future<void> main(FutureOr<void> testMain()) async {
   await loadAppFonts();
@@ -13,7 +14,8 @@ Future<void> main(FutureOr<void> testMain()) async {
 Future<void> loadAppFonts() async {
   TestWidgetsFlutterBinding.ensureInitialized();
   final fontLoader = FontLoader('Roboto');
-  final fontData = File('fonts/Roboto-Regular.ttf')
+
+  final fontData = File(p.join('fonts', 'Roboto-regular.ttf'))
       .readAsBytes()
       .then((bytes) => ByteData.view(Uint8List.fromList(bytes).buffer));
   fontLoader.addFont(fontData);
